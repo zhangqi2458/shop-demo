6cd27bb0bd245ab71c6cdf5e3817d113d8ee86b0
zhangqi2458 <964996085@qq.com>
2018-05-16 18:07:06 +0800

    qiqi

---
+++ shop-demo/src/components/NavBread.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">Home</a>
+        <!--<span>Goods</span>-->
+        <slot name="bread"></slot>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {}
+        },
+    }
+</script>
+<style scoped>
+
+</style>
